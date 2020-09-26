faf00d8cec823ebae8ce4460eba845c58f23a060
Sander Huijsen <sander.huijsen@nmtafe.wa.edu.au>
2020-09-26 18:17:21 +0800

    Added __init__.py to colourise. (Oops.) Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='colourise',
-    version='0.0.2',
+    version='0.0.3',
     author='Sander Huijsen',
     author_email='sander.huijsen@nmtafe.wa.edu.au',
     description='Package performing colour conversions',
     long_description=long_description,
     url='https://github.com/Sandyman/colourise',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['*.test', 'test']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
